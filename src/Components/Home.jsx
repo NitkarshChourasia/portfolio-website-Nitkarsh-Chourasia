806d52a794490efc624416a80a10144280e9a072
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2024-05-06 08:54:59 +0000

    deploy: Made basic changes to test deploy #1.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/glass_and_code.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Two windows of codes. One written in Video editor and another in terminal. With glasses watching each of them.";
 
 const Home = ({ name, title }) => {
   return (
